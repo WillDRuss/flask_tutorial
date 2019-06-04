5abec160ab15b93632cafa8deab8884f13587746
WillDRuss <willrussell90@hotmail.com>
2019-06-04 18:20:17 +0100

    adding final database updates

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -5,25 +5,25 @@ from app.forms import LoginForm
 @app.route('/')
 @app.route('/index')
 def index():
-	user = {'username': 'Miguel'}
-	posts = [
-		{
-			'author': {'username': 'John'},
-			'body': 'Beautiful day in Portland!'
-		},
-		{
-			'author': {'username': 'Susan'},
-			'body': 'The Avengers movie was so cool!'
-		}
-	]
-	return render_template('index.html', user=user, posts=posts)
+    user = {'username': 'Miguel'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', user=user, posts=posts)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-	form = LoginForm()
-	if form.validate_on_submit():
-		flash('Login requested for user {}, remember_me={}'.format(
-			form.username.data, form.remember_me.data))
-		return redirect(url_for('index'))
-	return render_template('login.html', title='Sign In', form=form)
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
 
